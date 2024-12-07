e23465756b71e68acdbc64b4d2e7069fe1de2bd8
dewandra <you@example.com>
2024-12-07 18:45:30 +0700

    muat gambar

--- resources/views/assignment/dashboard/index.blade.php
+++ resources/views/assignment/dashboard/index.blade.php
@@ -5,6 +5,8 @@
     <!-- Meta Tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
 
     <!-- Title of the page -->
     <title>SIMS Web App</title>
